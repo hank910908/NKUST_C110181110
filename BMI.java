a676738b4da89b3a6bc0b7037b93b8e5a7baeaba
C110181110 <93572249+hank910908@users.noreply.github.com>
2021-11-09 16:42:19 +0800

    Add files via upload

---
+++ BMI.java
@@ -0,0 +1,8 @@
+public class BMI {
+    public static void main(String[]args){
+        float height = 1.74f;
+        int weight = 68;
+        float bmi=(weight)/(height*height);
+        System.out.println(bmi);
+    }
+}
