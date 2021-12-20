e955633d7d0abba19a5ded899d56161882b86b96
C110181110 <93572249+hank910908@users.noreply.github.com>
2021-12-21 01:39:53 +0800

    Add files via upload

---
+++ IdeaProjects/game/src/game2.java
@@ -0,0 +1,10 @@
+public class game2 {
+    public static void main(String[] args) {
+        Person2 Archer = new Person2("Archer", 300, 780,799);
+        Archer.getCurrentStatus();
+        Person2 Berserker = new Person2( "Berserker", 850, 600,500);
+        Berserker.getCurrentStatus();
+        Person2 Magician = new Person2("Magician",450 ,580 ,1000);
+        Magician.getCurrentStatus();
+    }
+}
